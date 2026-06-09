e102e75855809de763c5a9d68a065ce5b4ff5135
zenithmc2026-png <zenithmc2026@gmail.com>
2026-06-09 04:09:31 +0000

    make horizontal portfolio

--- script.js
+++ script.js
@@ -1,56 +1,62 @@
-const glow = document.querySelector(".cursor-glow");
+const cursor=document.querySelector(".cursor");
 
-document.addEventListener("mousemove",(e)=>{
-glow.style.left=e.clientX+"px";
-glow.style.top=e.clientY+"px";
+document.addEventListener("mousemove",e=>{
+  cursor.style.left=e.clientX+"px";
+  cursor.style.top=e.clientY+"px";
 });
 
-const canvas=document.getElementById("snow");
+const canvas=document.getElementById("stars");
 const ctx=canvas.getContext("2d");
 
-let w,h;
+let w,h,stars=[];
 
 function resize(){
-w=canvas.width=window.innerWidth;
-h=canvas.height=window.innerHeight;
+  w=canvas.width=innerWidth;
+  h=canvas.height=innerHeight;
 }
-
-window.addEventListener("resize",resize);
 resize();
+addEventListener("resize",resize);
 
-const flakes=[];
-
-for(let i=0;i<120;i++){
-flakes.push({
-x:Math.random()*w,
-y:Math.random()*h,
-r:Math.random()*3+1,
-s:Math.random()*1.5+0.5
-});
+for(let i=0;i<180;i++){
+  stars.push({
+    x:Math.random()*w,
+    y:Math.random()*h,
+    r:Math.random()*2+1,
+    s:Math.random()*1.5+.4
+  });
 }
 
 function animate(){
+  ctx.clearRect(0,0,w,h);
+  ctx.fillStyle="white";
 
-ctx.clearRect(0,0,w,h);
+  stars.forEach(star=>{
+    ctx.beginPath();
+    ctx.arc(star.x,star.y,star.r,0,Math.PI*2);
+    ctx.fill();
 
-ctx.fillStyle="white";
+    star.x-=star.s;
+    star.y+=star.s*.25;
 
-flakes.forEach(f=>{
+    if(star.x<0){
+      star.x=w;
+      star.y=Math.random()*h;
+    }
+  });
 
-ctx.beginPath();
-ctx.arc(f.x,f.y,f.r,0,Math.PI*2);
-ctx.fill();
-
-f.y+=f.s;
-
-if(f.y>h){
-f.y=-10;
-f.x=Math.random()*w;
+  requestAnimationFrame(animate);
 }
-
-});
-
-requestAnimationFrame(animate);
-}
-
 animate();
+
+const music=document.getElementById("bgMusic");
+const btn=document.getElementById("musicBtn");
+
+btn.onclick=()=>{
+  if(music.paused){
+    music.play();
+    btn.textContent="⏸ Pause Background Music";
+  }else{
+    music.pause();
+    btn.textContent="▶ Play Background Music";
+  }
+};
